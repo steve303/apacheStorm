af8917f644e12bd43b132a8a8fb42ccc671ab609
steve303 <susc@colorado.edu>
2021-04-24 17:58:05 +0000

    none

--- multilang/resources/file_reader_spout.py
+++ multilang/resources/file_reader_spout.py
@@ -5,7 +5,7 @@ from time import sleep
 # from streamparse import Spout
 import storm
 
-
+FILENAME = "/tmp/data.txt"
 class FileReaderSpout(storm.Spout):
 
     def initialize(self, conf, context):
@@ -17,14 +17,19 @@ class FileReaderSpout(storm.Spout):
 
         # TODO:
         # Task: Initialize the file reader
-        pass
+        with open(FILENAME) as f:
+          self.lines = [line.strip() for line in f]
+        
         # End
 
     def nextTuple(self):
         # TODO:
         # Task 1: read the next line and emit a tuple for it
         # Task 2: don't forget to sleep for 1 second when the file is entirely read to prevent a busy-loop
-        pass
+        sleep(1)
+        for sentence in self.lines:
+          storm.logInfo("Emitting %s" % sentence)
+          storm.emit([sentence])
         # End
 
 
